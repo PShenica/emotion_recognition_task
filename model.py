0fa7380b34c19b940e90ef233027ec51ca92fd3c
PShenica <54884007+PShenica@users.noreply.github.com>
2020-01-06 20:00:22 +0500

    Add files via upload

--- model.py
+++ model.py
@@ -1,14 +1,16 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import seaborn as sn
 import os
 import keras
+import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Flatten, Dropout
 from keras.layers import Conv2D, MaxPooling2D
 from keras.preprocessing import image
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 
 # fix random seed
 random.seed(0)
@@ -22,32 +24,37 @@ image_size = (64, 64)
 train_bath_size = 32
 test_bath_size = 14
 
-steps_per_epoch = 40  # train image count / train bath size = steps per epoch
-validation_steps = 19  # val image count / test bath size = validation steps
+steps_per_epoch = 32  # train image count / train bath size = steps per epoch
+validation_steps = 16  # val image count / test bath size = validation steps
 
-epochs = 15
+epochs = 10
 
 train_generator = image.ImageDataGenerator().flow_from_directory(
     "dataset/spectrogram_png/train",
     classes=class_names,
     target_size=image_size,
-    # color_mode='grayscale',
-    batch_size=train_bath_size)
+    color_mode='grayscale',
+    batch_size=train_bath_size,
+    # class_mode='categorical',
+    # shuffle=False
+    )
 
 test_generator = image.ImageDataGenerator().flow_from_directory(
     "dataset/spectrogram_png/validation",
     classes=class_names,
     target_size=image_size,
-    # color_mode='grayscale',
-    batch_size=test_bath_size)
+    color_mode='grayscale',
+    batch_size=test_bath_size,
+    # class_mode='categorical',
+    shuffle=False
+    )
 
 # building model
 model = Sequential()
 
-model.add(Conv2D(16, (3, 3), activation = 'relu', padding = 'same', input_shape = (image_size[0], image_size[1], 3)))
+model.add(Conv2D(16, (3, 3), activation = 'relu', padding = 'same', input_shape = (image_size[0], image_size[1], 1)))
 model.add(Conv2D(16, (3, 3), activation = 'relu', padding = 'same', name = 'block0_conv2'))
 model.add(MaxPooling2D((2, 2), strides = (2, 2), name = 'block0_pool1'))
-model.add(Dropout(0.2, name = 'Dropout_1'))
 
 model.add(Conv2D(32, (3, 3), activation = 'relu', padding = 'same', name = 'block1_conv1'))
 model.add(Conv2D(32, (3, 3), activation = 'relu', padding = 'same', name = 'block1_conv2'))
@@ -57,7 +64,6 @@ model.add(MaxPooling2D((2, 2), strides = (2, 2), name = 'block1_pool1'))
 model.add(Conv2D(64, (3, 3), activation = 'relu', padding = 'same', name = 'block2_conv1'))
 model.add(Conv2D(64, (3, 3), activation = 'relu', padding = 'same', name = 'block2_conv2'))
 model.add(MaxPooling2D((2, 2), strides = (2, 2), name = 'block2_pool1'))
-model.add(Dropout(0.4, name = 'Dropout_3'))
 
 model.add(Conv2D(128, (3, 3), activation = 'relu', padding = 'same', name = 'block3_conv1'))
 model.add(Conv2D(128, (3, 3), activation = 'relu', padding = 'same', name = 'block3_conv2'))
@@ -67,7 +73,7 @@ model.add(MaxPooling2D((2, 2), strides = (2, 2), name = 'block3_pool1'))
 model.add(Flatten())
 model.add(Dense(128, activation = 'relu'))
 model.add(Dropout(0.4, name = 'Dropout_5'))
-model.add(Dense(num_classes, activation = 'sigmoid'))
+model.add(Dense(num_classes, activation = 'softmax'))
 
 model.summary()
 
@@ -83,6 +89,7 @@ cnn_history = model.fit_generator(
     verbose=2)
 
 # evaluate the model
+test_generator.reset()
 evaluated_model = model.evaluate_generator(generator=test_generator, steps=validation_steps)
 print(evaluated_model)
 
@@ -110,4 +117,33 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
+
+# confusion matrix and classification report
+test_generator.reset()
+Y_pred = model.predict_generator(test_generator, validation_steps + 1)
+y_pred = np.argmax(Y_pred, axis=1)
+
+print('Confusion Matrix')
+confusion_matrix = confusion_matrix(test_generator.classes, y_pred)
+print(confusion_matrix)
+
+print('Classification Report')
+classification_report = classification_report(test_generator.classes, y_pred, target_names=class_names)
+print(classification_report)
+
+# plt confusion matrix
+df_cm = pd.DataFrame(confusion_matrix,
+                     index = class_names,
+                     columns = class_names)
+
+sn.set(font_scale=1.4)  # for label size
+sn.heatmap(df_cm,
+           annot=True,
+           annot_kws={"size": 16},  # font size
+           cmap="RdPu")
+
+plt.show()
+
+
+
 
